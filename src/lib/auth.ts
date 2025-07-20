836ee3bcf665270bc0e699f2b0c28bf9c0f90545
aprizaprasetio <aprizaprasetio@gmail.com>
2025-07-20 16:38:24 +0700

    🔧 added better auth instance

---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "~/lib/db";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+});
+
+export type AuthType = {
+	user: typeof auth.$Infer.Session.user | null;
+	session: typeof auth.$Infer.Session.session | null;
+};
