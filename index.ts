514b5da6dff4b1860033ef867586f6df51b27366
aprizaprasetio <aprizaprasetio@gmail.com>
2025-07-20 16:36:09 +0700

    ✨ just hono init

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+
+const app = new Hono({
+	strict: false,
+});
+
+app.get("/", (c) => {
+	return c.text("Hello via Bun!", 400);
+});
+
+export default app;
